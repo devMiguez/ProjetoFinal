7fc25386ff72ad14ded9d763187e56a88d94300e
joao <joaovmiguez@gmail.com>
2023-12-01 12:25:50 -0300

    first commit

---
+++ sistemabancario/src/main/java/com/projetofinal/sistemabancario/SistemabancarioApplication.java
@@ -0,0 +1,13 @@
+package com.projetofinal.sistemabancario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemabancarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemabancarioApplication.class, args);
+	}
+
+}
